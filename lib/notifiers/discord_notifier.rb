280d6ae072958577162d575e21dff4c3e94e8734
Tanveer Najib <tanveernajib@gmail.com>
2023-12-29 15:32:23 +0000

    Fuck this I give up

--- lib/notifiers/discord_notifier.rb
+++ lib/notifiers/discord_notifier.rb
@@ -11,7 +11,7 @@ module Notifiers
             'title': "#{manga.name}",
             'description': "New chapter is available! \n#{manga.last_chapter}",
             'thumbnail': {
-              'url': Rails.application.routes.url_helpers.image_manga_url(manga, host: ENV["MANGA_NOTIFIER_HOST"] || "localhost")
+              'url': manga.image
             },
             'color': 28915
           }
