f6857f3dea5ba7e8000be72c9a8e620a82ff594b
Tanveer Najib <tanveernajib@gmail.com>
2023-12-29 14:30:28 +0000

    Move configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,6 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: ENV["MANGA_NOTIFIER_HOST"] }
-  Rails.application.routes.default_url_options[:host] = ENV["MANGA_NOTIFIER_HOST"]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -94,3 +93,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+Rails.application.routes.default_url_options[:host] = ENV["MANGA_NOTIFIER_HOST"]
