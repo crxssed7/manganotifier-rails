fc2bbbc42a684b4a5c6d194b8373707c04d030a4
Tanveer Najib <tanveernajib@gmail.com>
2023-12-29 18:32:26 +0000

    Wow

--- lib/notifiers/discord_notifier.rb
+++ lib/notifiers/discord_notifier.rb
@@ -8,14 +8,13 @@ module Notifiers
       Rails.logger.info("Notifying Discord webhook")
       Rails.logger.info("N_HOST: #{Rails.application.routes.default_url_options[:host]}")
       Rails.logger.info("N_PROTOCOL: #{Rails.application.routes.default_url_options[:protocol]}")
-      # Rails.application.routes.url_helpers.image_manga_url(manga)
       {
         'embeds': [
           {
             'title': "#{manga.name}",
             'description': "New chapter is available! \n#{manga.last_chapter}",
             'thumbnail': {
-              'url': manga.image
+              'url': Rails.application.routes.url_helpers.image_manga_url(manga)
             },
             'color': 28915
           }
