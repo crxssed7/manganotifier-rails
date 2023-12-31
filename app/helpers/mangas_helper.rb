a3cc99ca8d910b8c26b0842c05269c7d4599a6e6
Tanveer Najib <tanveernajib@gmail.com>
2023-12-31 18:08:42 +0000

    Zebra mode

--- app/helpers/mangas_helper.rb
+++ app/helpers/mangas_helper.rb
@@ -1,2 +1,11 @@
 module MangasHelper
+  def refreshed_ago(last_refreshed)
+    seconds_ago = (Time.current - last_refreshed).round
+    minutes_ago = seconds_ago / 60
+    hours_ago = minutes_ago / 60
+
+    return "#{hours_ago} hours" if minutes_ago > 60
+    return "#{minutes_ago} minutes" if seconds_ago > 60
+    "#{seconds_ago} seconds"
+  end
 end
--- app/javascript/controllers/custom_colour_controller.ts
+++ app/javascript/controllers/custom_colour_controller.ts
@@ -2,38 +2,40 @@ import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="custom-colour"
 export default class extends Controller {
-  static targets = ['colour']
+  static targets = ['background', 'border', 'stripe']
 
-  declare readonly colourTargets: HTMLElement[]
+  declare readonly backgroundTargets: HTMLElement[]
+  declare readonly borderTargets: HTMLElement[]
+  declare readonly stripeTargets: HTMLElement[]
 
   connect (): void {
-    this.updateColours()
+    this.update(this.borderTargets, this.setBorderColour)
+    this.update(this.backgroundTargets, this.setBackgroundColour)
+    this.update(this.stripeTargets, this.setStripes)
   }
 
-  updateColours (): void {
-    this.colourTargets.forEach((element) => {
+  setColour (element: HTMLElement, colour: string) {
+    element.style.color = colour
+  }
+
+  setBackgroundColour (element: HTMLElement, colour: string) {
+    element.style.backgroundColor = colour
+  }
+
+  setBorderColour (element: HTMLElement, colour: string) {
+    element.style.borderColor = colour
+  }
+
+  setStripes (element: HTMLElement, colour: string) {
+    element.style.background = `repeating-linear-gradient(45deg, whitesmoke, whitesmoke 55px, ${colour} 55px, ${colour} 110px)`
+  }
+
+  update (targets: HTMLElement[], setColourCallback: (element: HTMLElement, colour: string) => void): void {
+    targets.forEach((element) => {
       const colour = element.getAttribute('data-colour')
       if (colour === null) return
 
-      element.addEventListener(
-        'mouseenter',
-        (event: Event) => {
-          const currentTarget = event.currentTarget as HTMLElement | null
-          if (currentTarget === null) return
-          currentTarget.style.backgroundColor = colour
-        },
-        false
-      )
-
-      element.addEventListener(
-        'mouseleave',
-        (event: Event) => {
-          const currentTarget = event.currentTarget as HTMLElement | null
-          if (currentTarget === null) return
-          currentTarget.style.backgroundColor = 'whitesmoke'
-        },
-        false
-      )
+      setColourCallback(element, colour)
     })
   }
 }
