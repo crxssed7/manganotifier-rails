512134d998110fcf8248cf44e7dfa637d39a980f
Tanveer Najib <tanveernajib@gmail.com>
2024-08-30 12:07:24 +0100

    Check for puma when running discord bot

--- config/initializers/discord.rb
+++ config/initializers/discord.rb
@@ -1,5 +1,5 @@
 Rails.application.config.after_initialize do
-  if defined?(Rails::Server)
+  if defined?(Rails::Server) || File.basename($PROGRAM_NAME).include?("puma")
     Thread.new do
       Discord::Bot.new.run
     end
