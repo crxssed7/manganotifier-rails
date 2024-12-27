e2a5d05731f0045ba017bc32cd13cac3b8bf4e6e
Tanveer Najib <tanveernajib@gmail.com>
2024-12-27 22:54:33 +0000

    Add sidekiq initializer

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,23 @@
+db = {
+  "development" => 0,
+  "production" => 0,
+  # Avoid test runs interfering with development (15 is highest database index)
+  "test" => 15
+}.fetch(Rails.env.to_s)
+
+sidekiq_config = {url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1"), db: db}
+
+Sidekiq.configure_server do |config|
+  config.logger.level = :debug
+  config.redis = sidekiq_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = sidekiq_config
+end
+
+Sidekiq.default_job_options = {"backtrace" => true}
+
+if Rails.env.test?
+  Sidekiq.logger.level = Logger::WARN
+end
